4ef66ef17760228374dbcd66e96b1ed31b2ce903
ivynya <vyne@ivy.direct>
2022-12-27 14:59:07 -0800

    repo: update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 
-import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
-export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
