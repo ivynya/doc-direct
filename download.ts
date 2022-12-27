e68206e666a33e005d55b14f2b9845b0b61e797e
ivynya <vyne@ivy.direct>
2022-12-27 14:48:39 -0800

    repo: rm static caching & use templating instead

---
+++ download.ts
@@ -0,0 +1,13 @@
+import { NotionToMarkdown } from "npm:notion-to-md";
+
+export async function getPageMarkdown(notion: any, pageId: string) {
+  // get page title
+  const page = await notion.pages.retrieve({ page_id: pageId });
+  const title = page.properties.title.title[0].text.content;
+  // get page content
+  const n2m = new NotionToMarkdown({ notionClient: notion });
+  const mdblocks = await n2m.pageToMarkdown(pageId);
+  const mdString = n2m.toMarkdownString(mdblocks);
+
+  return `# ${title}\n\n` + mdString;
+}
--- main.ts
+++ main.ts
@@ -1,50 +1,27 @@
+import { Application, Router } from "./deps.ts";
+import { getPageMarkdown } from "./download.ts";
 
-import { Application, Router, send } from "./deps.ts";
+import { Client } from "npm:@notionhq/client";
+const notion = new Client({ auth: Deno.env.get("NOTION_TOKEN") });
 
 /* Application logic */
 const app = new Application();
 const router = new Router();
 
-function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 router
   .get("/d/:id", ctx => {
-    Deno.run({ cmd: ["node", "./download.js", '-p', ctx.params.id] });
-    ctx.response.body = "OK";
+    ctx.response.body = getPageMarkdown(notion, ctx.params.id);
   })
   .get("/p/:id", async ctx => {
     const index = `${Deno.cwd()}/content/index.html`;
-    const md = `${Deno.cwd()}/content/${ctx.params.id}.md`;
-    try {
-      await Deno.readFile(md);
-      ctx.response.body = await Deno.readFile(index);
-      // Update file
-      Deno.run({ cmd: ["node", "./download.js", '-p', ctx.params.id] });
-    }
-    catch (e) {
-      Deno.run({ cmd: ["node", "./download.js", '-p', ctx.params.id] });
-      timeout(1000);
-      ctx.response.body = await Deno.readFile(index);
-    }
+    const md = await getPageMarkdown(notion, ctx.params.id);
+    let html = await Deno.readTextFile(index);
+    html = html.replaceAll("%REPLACE_MARKDOWN_STRING%", md);
+    ctx.response.body = html;
   });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-/* Static file serving (publish widget + usercontent) */
-app.use(async (ctx) => {
-  try {
-    await send(ctx, ctx.request.url.pathname, {
-      root: `${Deno.cwd()}/content`,
-      index: 'index.html',
-    });
-  } catch (e) {
-    console.log(`[ERR] ${e.name} - ${ctx.request.url}`);
-    ctx.response.status = 404;
-  }
-})
 
 /* Start server */
 console.log(`[EVT] Listening on :8000`);
