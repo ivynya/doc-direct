41e94c91b4c41c990e03d0d0f7d8ce8e76906110
ivynya <vyne@ivy.direct>
2022-12-27 16:04:22 -0800

    repo: workaround

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Use Notion to publish a document to the web with custom URL and branding.
 
+> ⚠️ Uses unofficial notion-to-md and @notionhq/client packages to allow for Deno Deploy. Will be removed once Deploy supports NPM. 
+
 ## Deploying Doc Direct
 
 1. Create a Notion integration from the developers console
--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 export { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
-export { Client } from "npm:@notionhq/client";
-export { NotionToMarkdown } from "npm:notion-to-md";
+export { Client } from "https://deno.land/x/notion_sdk@v1.0.4/src/mod.ts";
+export { NotionToMarkdown } from "https://deno.land/x/notion_to_md@v2.5.0/mod.ts";
